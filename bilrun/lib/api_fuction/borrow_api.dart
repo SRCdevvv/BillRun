32c59de109ba2246b7c821fb03b6a9870eaf6d67
KIM WONHEE <soyoung0007@naver.com>
2021-02-04 14:52:24 +0900

    Update borrow_api.dart

--- bilrun/lib/api_fuction/borrow_api.dart
+++ bilrun/lib/api_fuction/borrow_api.dart
@@ -20,23 +20,26 @@ class BorrowApi extends StatefulWidget {
 
 }
 
+Future<String> getData() async {
+  http.Response response = await http.get(
+      Uri.encodeFull('http://172.30.1.17:8000/api/lend_product_list/'),
+      headers:{"Accept" : "application/json "});
+
+
+
+  dynamic datas = utf8.decode(response.bodyBytes);
+  List<dynamic> LendList = jsonDecode(datas);
+  for (var i=0 ; i<LendList.length; i++)i:
+  print(LendList[i]);
+
+
+}
+
+
+
+
 
 class _BorrowApiState extends State<BorrowApi> {
-
-  Future<String> getData() async {
-    http.Response response = await http.get(
-        Uri.encodeFull('http://172.30.1.17:8000/api/lend_product_list/'),
-        headers:{"Accept" : "application/json "});
-
-    dynamic datas = utf8.decode(response.bodyBytes);
-
-
-     List<dynamic> LendList = jsonDecode(datas);
-    for (var i=0 ; i<LendList.length; i++)i:
-      print(LendList[i]);
-
-
-  }
 
 
 
