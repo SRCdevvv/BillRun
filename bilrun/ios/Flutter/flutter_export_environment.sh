49713d1dff85a359a6654ded9b616831ececa37b
woonees2 <hello_world_@kakao.com>
2021-04-16 17:10:54 +0900

    .

--- bilrun/ios/Flutter/flutter_export_environment.sh
+++ bilrun/ios/Flutter/flutter_export_environment.sh
@@ -2,7 +2,7 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/kimwonhee/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/kimwonhee/Desktop/GitHub/BillRun/bilrun"
-export "FLUTTER_TARGET=/Users/kimwonhee/Desktop/GitHub/BillRun/bilrun/lib/api_function/borrow_api.dart"
+export "FLUTTER_TARGET=/Users/kimwonhee/Desktop/GitHub/BillRun/bilrun/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
