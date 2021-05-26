8b30175662af9084c7af785681bd6e27296351e9
woonees2 <hello_world_@kakao.com>
2021-05-26 16:29:10 +0900

    update appbar

--- bilrun/lib/screens/Rent/rent_main.dart
+++ bilrun/lib/screens/Rent/rent_main.dart
@@ -3,6 +3,7 @@ import 'package:bilrun/screens/mypage/deal_managemenet/deal_list_controller.dart
 import 'package:bilrun/screens/rent/rent_controller.dart';
 import 'package:bilrun/screens/rent/rent_service.dart';
 import 'package:bilrun/widgets/community/now_community.dart';
+import 'package:bilrun/widgets/location/controller_location.dart';
 import 'package:bilrun/widgets/main_drawer.dart';
 import 'package:bilrun/widgets/search/search_button.dart';
 import 'package:flutter/material.dart';
@@ -12,12 +13,9 @@ import 'package:bilrun/widgets/location/now_location.dart';
 import 'package:bilrun/design/divider_example.dart';
 import 'package:bilrun/screens/Rent/rent_product_list.dart';
 
-
-
-void main()=>runApp(RentMain());
+void main() => runApp(RentMain());
 
 class RentMain extends StatefulWidget {
-
   RentMain({Key key, this.title}) : super(key: key);
 
   final String title;
@@ -27,155 +25,131 @@ class RentMain extends StatefulWidget {
 }
 
 class _RentMainState extends State<RentMain> {
+  RentProductController rentProductController =
+      Get.put(RentProductController());
 
-  RentProductController rentProductController = Get.put(RentProductController());
+  Future<Null> refresh() async {
+    RentProductListService.fetchRentProducts();
+    RentProductController.rentfetchProducts();
+    rentProductController = Get.put(RentProductController());
+  }
 
-   Future<Null> refresh() async{
-     RentProductListService.fetchRentProducts();
-     RentProductController.rentfetchProducts();
-     rentProductController = Get.put(RentProductController());
-
-   }
-
-
-
-
-
+  static String fullLocation;
 
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
-
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(52),
-        child:
-        AppBar(
-
-          iconTheme: IconThemeData(
-            color: Colors.black,
-          ),
+        child: AppBar(
           backgroundColor: Colors.white,
-          title: Row(
-            children: <Widget>[
-              Image.asset('assets/images/logo.png', height: 40, width: 100)
-            ],
-          ),
+          elevation: 0,
+          title: FutureBuilder(
+              future: LocationDataController.fetchLocationData(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                }
+
+                if (snapshot.hasError) {
+                  return Text("banner error ${snapshot.hasError}");
+                } else {
+                  var location =
+                      LocationDataController.locationData.value.results[0];
+                  print(location);
+                  fullLocation = location.formattedAddress;
+                  fullLocation = fullLocation.substring(5);
+                }
+                return Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15),
+                      child: Text(
+                        "$fullLocation",
+                        style: const TextStyle(
+                            color: const Color(0xff191919),
+                            fontWeight: FontWeight.w700,
+                            fontFamily: "NotoSansCJKkr",
+                            fontStyle: FontStyle.normal,
+                            fontSize: 16.0),
+                      ),
+                    ),
+                    IconButton(
+                        icon: Icon(
+                          Icons.keyboard_arrow_down_outlined,
+                          color: Colors.black,
+                          size: 25,
+                        ),
+                        onPressed: () {
+                          Get.to(null);
+                        })
+                  ],
+                );
+              }),
           actions: <Widget>[
             Padding(
-              padding: EdgeInsets.fromLTRB(10, 0, 20, 0),
+              padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
               child: SearchButton(),
             ),
-
-            //TODO 검색창으로 넘어가도록 네비게이터
           ],
         ),
-
       ),
-
-
-      body:
-      RefreshIndicator(
+      body: RefreshIndicator(
         onRefresh: refresh,
         child: SafeArea(
           child: Column(
             children: [
-
-
-                         ListView(
-
+              ListView(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                children: [
+                  OriginDivider(mainRed, 100, 0, 0),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                          child: NowCommunity(),
+                        ),
+                        Text("지금 빌려주세요!",
+                            style: const TextStyle(
+                                color: const Color(0xff191919),
+                                fontWeight: FontWeight.w700,
+                                fontFamily: "NotoSansCJKkr",
+                                fontStyle: FontStyle.normal,
+                                fontSize: 16.0),
+                            textAlign: TextAlign.left)
+                      ],
+                    ),
+                  ),
+                  Obx(() {
+                    if (RentProductController.isLoading.value)
+                      return Center(child: CircularProgressIndicator());
+                    else
+                      return ListView.builder(
                           scrollDirection: Axis.vertical,
                           shrinkWrap: true,
-                          children: [
-                            OriginDivider(mainRed, 100, 0, 0),
-                           
-                                 Padding(
-                                  padding: const EdgeInsets.fromLTRB(0,20,0,0),
-                                  child: Row(
-                                    children: [
-                                      Padding(padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                                       child: NowCommunity(),
-                                      ),
-                                      Text(
-                                          "지금 빌려주세요!",
-                                          style: const TextStyle(
-                                              color:  const Color(0xff191919),
-                                              fontWeight: FontWeight.w700,
-                                              fontFamily: "NotoSansCJKkr",
-                                              fontStyle:  FontStyle.normal,
-                                              fontSize: 16.0
-                                          ),
-                                          textAlign: TextAlign.left
-                                      )
-                                    ],
-                                  ),
-
+                          itemCount:
+                              RentProductController.rentProductList.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Column(children: [
+                              RentProductTile(
+                                  RentProductController.rentProductList[index]),
+                              Container(
+                                height: 1,
+                                color: Color(0xffdedede),
+                                width: Get.width * 0.867,
                               ),
-                              Obx(()
-                              {
-                                if (RentProductController.isLoading.value)
-                                  return Center(child: CircularProgressIndicator());
-                                else
-
-                                  return
-                                    ListView.builder(
-                                        scrollDirection: Axis.vertical,
-                                        shrinkWrap: true,
-                                        itemCount: RentProductController.rentProductList.length,
-
-
-                                        itemBuilder: ( BuildContext context, int index){
-
-                                          return Column(
-                                          children: [
-
-                                          RentProductTile(RentProductController.rentProductList[index]),
-                                            Container(height: 1, color: Color(0xffdedede),width: Get.width * 0.867,),
-
-                                          ]
-                                          );
-
-                                        });
-                              }
-
-                              ),
-
-
-                          ],
-                        ),
-
-
-
-
+                            ]);
+                          });
+                  }),
                 ],
-
               ),
+            ],
           ),
         ),
-
-
-      drawer: Drawer(
-        child:
-        MainDrawer(),
       ),
-
-
-
-
     );
-
-
-
-
-
-
-
-
-
   }
-
-
-
 }
-
--- bilrun/lib/screens/lend/lend_main.dart
+++ bilrun/lib/screens/lend/lend_main.dart
@@ -1,6 +1,7 @@
 import 'package:bilrun/design/usedColors.dart';
 import 'package:bilrun/widgets/banner.dart';
 import 'package:bilrun/widgets/community/now_community.dart';
+import 'package:bilrun/widgets/location/controller_location.dart';
 import 'package:bilrun/widgets/main_drawer.dart';
 import 'package:bilrun/widgets/notice/notice_banner.dart';
 import 'package:bilrun/widgets/notice/notice_controller.dart';
@@ -14,12 +15,9 @@ import 'package:bilrun/widgets/location/now_location.dart';
 import 'package:bilrun/design/divider_example.dart';
 import 'package:bilrun/screens/lend/lend_service.dart';
 
-
-
-void main()=>runApp(LendMain());
+void main() => runApp(LendMain());
 
 class LendMain extends StatefulWidget {
-
   LendMain({Key key, this.title}) : super(key: key);
 
   final String title;
@@ -28,217 +26,203 @@ class LendMain extends StatefulWidget {
   _LendMainState createState() => _LendMainState();
 }
 
-
-
-List<String> noticeImgList=[];
+List<String> noticeImgList = [];
 
 class _LendMainState extends State<LendMain> {
-  bool isTaped=false;
-
-
+  bool isTaped = false;
+  static String fullLocation;
 
   LendProductController productController = Get.put(LendProductController());
 
-
-
-
-Future<Null> refresh() async{
-  ProductListService.fetchLendProducts();
-  LendProductController.fetchProducts();
-  this.productController = Get.put(LendProductController());
-}
-
+  Future<Null> refresh() async {
+    ProductListService.fetchLendProducts();
+    LendProductController.fetchProducts();
+    this.productController = Get.put(LendProductController());
+  }
 
   @override
   Widget build(BuildContext context) {
-  //print("lend main : ${LendProductController.productList}");
-
-      return
-        Scaffold(
-
-        appBar: AppBar(
-
-          iconTheme: IconThemeData(
-            color: Colors.black,
-          ),
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(52),
+        child: AppBar(
           backgroundColor: Colors.white,
-          title: Row(
-            // mainAxisAlignment: MainAxisAlignment.start,
-            // mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Image.asset('assets/images/logo.png', height: 40, width: 100)
-            ],
-          ),
-          actions: <Widget>[
-            Padding(
-              padding: EdgeInsets.fromLTRB(10, 0, 20, 0),
-              child: SearchButton(),
-            ),
+          elevation: 0,
+          title: FutureBuilder(
+              future: LocationDataController.fetchLocationData(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                }
 
-            //TODO 검색창으로 넘어가도록 네비게이터
-          ],
-        ),
-
-        body:
-            RefreshIndicator(
-              onRefresh: refresh,
-
-              child:  SafeArea(
-                child: Column(
+                if (snapshot.hasError) {
+                  return Text("banner error ${snapshot.hasError}");
+                } else {
+                  var location =
+                      LocationDataController.locationData.value.results[0];
+                  print(location);
+                  fullLocation = location.formattedAddress;
+                  fullLocation = fullLocation.substring(5);
+                }
+                return Row(
                   children: [
-                    Container(
-                      child: Expanded(
-                        child: ListView(
-                          scrollDirection: Axis.vertical,
-                          shrinkWrap: true,
-                          children: [
-
-                            Padding(
-                              padding: const EdgeInsets.only(top:8.0),
-                              child: FutureBuilder(
-                                future: NoticeController.NoticeFetchList(),
-                                    builder: (BuildContext context, AsyncSnapshot snapshot) {
-                                      if (snapshot.connectionState ==
-                                          ConnectionState.waiting) {
-                                        return Center(child: CircularProgressIndicator());
-                                      }
-
-                                      if (snapshot.hasError) {
-                                        return Text("notice error");
-                                      }
-                                      else{
-                                        noticeImgList.clear();
-                                        for(int i=0; i<NoticeController.noticeLists.length; i++){
-                                          noticeImgList.add(NoticeController.noticeLists[i].bannerPhoto);
-                                        }
-                                        return noticeBannerWidget();
-                                      }
-                                    }
-
-                              ),
-                            ),
-                                  
-                
-                            GestureDetector(
-                              child: Padding(
-                                padding: const EdgeInsets.fromLTRB(240,10,10,10),
-                                child: Container(
-                                  width: Get.width ,
-                                  height: Get.height * 0.03,
-                                  child: Row(
-                                    children: [
-                                      Icon(Icons.account_balance_outlined,color: isTaped ==false ? Color(0xff767676) : Color(0xffaa0000),size: 20,),
-
-                                      Text("주변 학교까지 보기",
-                                        style : TextStyle(
-                                            color:  isTaped ==false ? Color(0xff767676) : Color(0xffaa0000),
-                                            fontWeight: FontWeight.bold,
-                                            fontFamily: "NotoSansCJKkr",
-                                            fontSize: 14.0
-                                        ),
-                                        textAlign: TextAlign.right,),
-
-                                    ],
-                                  ),
-                                ),
-                              ),
-                              onTap: (){
-                                setState(() {
-                                  isTaped == true ? isTaped= false : isTaped = true;
-                                });
-                              },
-                            ),
-                            Row(
-                              children: [
-                                Padding(padding: EdgeInsets.fromLTRB(10, 0, 5, 0),
-                                  child: NowCommunity(),
-                                ),
-                                // 지금 빌려드려요!
-                                Text(
-                                    "지금 빌려드려요!",
-                                    style: const TextStyle(
-                                        color:  const Color(0xff191919),
-                                        fontWeight: FontWeight.w700,
-                                        fontFamily: "NotoSansCJKkr",
-                                        fontStyle:  FontStyle.normal,
-                                        fontSize: 16.0
-                                    ),
-                                    textAlign: TextAlign.left
-                                )
-                              ],
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(24,10,24,0),
-                              child: Obx(()
-                              {
-                                if (LendProductController.isLoading.value)
-                                  return Center(child: CircularProgressIndicator());
-
-                                else
-                                  return
-                                    GridView.count(
-                                      physics: ScrollPhysics(),
-                                      childAspectRatio: (150/216),
-                                      crossAxisSpacing:12 ,
-                                      mainAxisSpacing: 20,
-                                      crossAxisCount: 2,
-                                      // childAspectRatio: (Get.width*0.134/Get.height),
-                                      scrollDirection: Axis.vertical,
-                                      shrinkWrap: true,
-
-                                      children: List.generate(
-                                          LendProductController.productList.length,
-
-
-                                              (index) {
-
-                                            return
-
-
-                                              LendProductTile(LendProductController.productList[index]);
-
-
-                                          }
-
-
-
-
-                                      ),
-                                    );
-                              }
-                              ),
-                            ),
-                          ],
-                        ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15),
+                      child: Text(
+                        "$fullLocation",
+                        style: const TextStyle(
+                            color: const Color(0xff191919),
+                            fontWeight: FontWeight.w700,
+                            fontFamily: "NotoSansCJKkr",
+                            fontStyle: FontStyle.normal,
+                            fontSize: 16.0),
                       ),
                     ),
+                    IconButton(
+                        icon: Icon(
+                          Icons.keyboard_arrow_down_outlined,
+                          color: Colors.black,
+                          size: 25,
+                        ),
+                        onPressed: () {
+                          Get.to(null);
+                        })
                   ],
+                );
+              }),
+          actions: <Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
+              child: SearchButton(),
+            ),
+          ],
+        ),
+      ),
+      body: RefreshIndicator(
+        onRefresh: refresh,
+        child: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                child: Expanded(
+                  child: ListView(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: FutureBuilder(
+                            future: NoticeController.NoticeFetchList(),
+                            builder:
+                                (BuildContext context, AsyncSnapshot snapshot) {
+                              if (snapshot.connectionState ==
+                                  ConnectionState.waiting) {
+                                return Center(
+                                    child: CircularProgressIndicator());
+                              }
+
+                              if (snapshot.hasError) {
+                                return Text("notice error");
+                              } else {
+                                noticeImgList.clear();
+                                for (int i = 0;
+                                    i < NoticeController.noticeLists.length;
+                                    i++) {
+                                  noticeImgList.add(NoticeController
+                                      .noticeLists[i].bannerPhoto);
+                                }
+                                return noticeBannerWidget();
+                              }
+                            }),
+                      ),
+                      GestureDetector(
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(240, 10, 10, 10),
+                          child: Container(
+                            width: Get.width,
+                            height: Get.height * 0.03,
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.account_balance_outlined,
+                                  color: isTaped == false
+                                      ? Color(0xff767676)
+                                      : Color(0xffaa0000),
+                                  size: 20,
+                                ),
+                                Text(
+                                  "주변 학교까지 보기",
+                                  style: TextStyle(
+                                      color: isTaped == false
+                                          ? Color(0xff767676)
+                                          : Color(0xffaa0000),
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: "NotoSansCJKkr",
+                                      fontSize: 14.0),
+                                  textAlign: TextAlign.right,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        onTap: () {
+                          setState(() {
+                            isTaped == true ? isTaped = false : isTaped = true;
+                          });
+                        },
+                      ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(10, 0, 5, 0),
+                            child: NowCommunity(),
+                          ),
+                          // 지금 빌려드려요!
+                          Text("지금 빌려드려요!",
+                              style: const TextStyle(
+                                  color: const Color(0xff191919),
+                                  fontWeight: FontWeight.w700,
+                                  fontFamily: "NotoSansCJKkr",
+                                  fontStyle: FontStyle.normal,
+                                  fontSize: 16.0),
+                              textAlign: TextAlign.left)
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(24, 10, 24, 0),
+                        child: Obx(() {
+                          if (LendProductController.isLoading.value)
+                            return Center(child: CircularProgressIndicator());
+                          else
+                            return GridView.count(
+                              physics: ScrollPhysics(),
+                              childAspectRatio: (150 / 216),
+                              crossAxisSpacing: 12,
+                              mainAxisSpacing: 20,
+                              crossAxisCount: 2,
+                              // childAspectRatio: (Get.width*0.134/Get.height),
+                              scrollDirection: Axis.vertical,
+                              shrinkWrap: true,
+
+                              children: List.generate(
+                                  LendProductController.productList.length,
+                                  (index) {
+                                return LendProductTile(
+                                    LendProductController.productList[index]);
+                              }),
+                            );
+                        }),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-        drawer: Drawer(
-          child:
-          MainDrawer(),
+            ],
+          ),
         ),
-
-
-
-
-
-      );
-
-
-
-
-
-
-
-
-
+      ),
+    );
+  }
 }
 
-
-
-}
 bool _isPressed = false;
-
