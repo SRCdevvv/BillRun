c08515be21b36f031a6d74e5aef4083d1777d519
woonees2 <hello_world_@kakao.com>
2021-06-10 15:43:59 +0900

    Update Product Review Page

--- bilrun/lib/widgets/create_review/product_review/create_product_review_page.dart
+++ bilrun/lib/widgets/create_review/product_review/create_product_review_page.dart
@@ -10,144 +10,171 @@ class CreateProductReview extends StatefulWidget {
   _CreateProductReviewState createState() => _CreateProductReviewState();
 }
 
+class InitData {
+  double rating;
+  String reviewText;
+}
+
 class _CreateProductReviewState extends State<CreateProductReview> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    // 상단 바 정보 받아오기
+    super.initState();
+  }
+
+  InitData data = new InitData();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
       body: SafeArea(
           child: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 38.0, bottom: 10),
-                child: Text("대여하신 물품은 어떠셨나요?",
-                    style: const TextStyle(
-                        color: const Color(0xff191919),
-                        fontWeight: FontWeight.w500,
-                        fontFamily: "NotoSansCJKkr",
-                        fontStyle: FontStyle.normal,
-                        fontSize: 20.0),
-                    textAlign: TextAlign.left),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 25.0, top: 10),
-                child: Text("후기를 작성하신 후 상대방이\n회원님에게 남긴 리뷰를 확인할 수 있어요!",
-                    style: const TextStyle(
-                        color: const Color(0xff000000),
-                        fontWeight: FontWeight.w400,
-                        fontFamily: "NotoSansCJKkr",
-                        fontStyle: FontStyle.normal,
-                        fontSize: 16.0),
-                    textAlign: TextAlign.center),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 38.0),
-                child: RatingBar.builder(
-                  itemSize: 45,
-                  glow: false,
-                  unratedColor: lightGrey,
-                  initialRating: 0,
-                  minRating: 1,
-                  direction: Axis.horizontal,
-                  allowHalfRating: true,
-                  itemCount: 5,
-                  itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
-                  itemBuilder: (context, _) => Icon(
-                    Icons.star,
-                    color: starYellow,
-                  ),
-                  onRatingUpdate: (rating) {
-                    print(rating);
-                  },
+        child: Form(
+          key: this.formKey,
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 38.0, bottom: 10),
+                  child: Text("대여하신 물품은 어떠셨나요?",
+                      style: const TextStyle(
+                          color: const Color(0xff191919),
+                          fontWeight: FontWeight.w500,
+                          fontFamily: "NotoSansCJKkr",
+                          fontStyle: FontStyle.normal,
+                          fontSize: 20.0),
+                      textAlign: TextAlign.left),
                 ),
-              ),
-              Container(
-                width: Get.width * 0.867,
-                height: Get.height * 0.27,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    border:
-                        Border.all(color: const Color(0xff999999), width: 1)),
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(44, 0, 24, 0),
-                  child: TextFormField(
-                    maxLines: 8,
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return '상품 설명은 필수 입력 항목입니다!';
-                      } else {
-                        return null;
-                      }
-                    },
-                    decoration: InputDecoration(
-                      hintText: "이용하신 대여 물품에 대한 리뷰를 남겨주세요.",
-                      hintStyle: TextStyle(
-                          color: const Color(0xff999999),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 25.0, top: 10),
+                  child: Text("후기를 작성하신 후 상대방이\n회원님에게 남긴 리뷰를 확인할 수 있어요!",
+                      style: const TextStyle(
+                          color: const Color(0xff000000),
                           fontWeight: FontWeight.w400,
                           fontFamily: "NotoSansCJKkr",
                           fontStyle: FontStyle.normal,
-                          fontSize: 14.0),
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.transparent),
-                      ),
-                      focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.transparent)),
+                          fontSize: 16.0),
+                      textAlign: TextAlign.center),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 38.0),
+                  child: RatingBar.builder(
+                    itemSize: 45,
+                    glow: false,
+                    unratedColor: lightGrey,
+                    initialRating: 0,
+                    minRating: 1,
+                    direction: Axis.horizontal,
+                    allowHalfRating: true,
+                    itemCount: 5,
+                    itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                    itemBuilder: (context, _) => Icon(
+                      Icons.star,
+                      color: starYellow,
                     ),
-                    cursorColor: mainGrey,
-                    onSaved: (String value) {
-                      //this.data.description = value;
+                    onRatingUpdate: (rating) {
+                      setState(() {
+                        this.data.rating = rating;
+                      });
                     },
                   ),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(right: 35.0, top: 10),
-                    child: Text("1000자 이내",
-                        style: const TextStyle(
+                Container(
+                  width: Get.width * 0.867,
+                  height: Get.height * 0.27,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      border:
+                          Border.all(color: const Color(0xff999999), width: 1)),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(44, 0, 24, 0),
+                    child: TextFormField(
+                      maxLines: 8,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return '리뷰 작성시 후기는 필수 입니다!';
+                        }
+                        if (value.length < 5) {
+                          return '후기는 5글자 이상 작성해주세요.';
+                        } else {
+                          return null;
+                        }
+                      },
+                      decoration: InputDecoration(
+                        hintText: "이용하신 대여 물품에 대한 리뷰를 남겨주세요.",
+                        hintStyle: TextStyle(
                             color: const Color(0xff999999),
                             fontWeight: FontWeight.w400,
                             fontFamily: "NotoSansCJKkr",
                             fontStyle: FontStyle.normal,
-                            fontSize: 12.0),
-                        textAlign: TextAlign.left),
+                            fontSize: 14.0),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.transparent)),
+                      ),
+                      cursorColor: mainGrey,
+                      onSaved: (String value) {
+                        setState(() {
+                          this.data.reviewText = value;
+                        });
+                      },
+                    ),
                   ),
-                ],
-              )
-            ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 35.0, top: 10),
+                      child: Text("1000자 이내",
+                          style: const TextStyle(
+                              color: const Color(0xff999999),
+                              fontWeight: FontWeight.w400,
+                              fontFamily: "NotoSansCJKkr",
+                              fontStyle: FontStyle.normal,
+                              fontSize: 12.0),
+                          textAlign: TextAlign.left),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       )),
-      bottomNavigationBar: admitButton(),
+      bottomNavigationBar: admitButton(() async {
+        if (formKey.currentState.validate()) {
+          this.formKey.currentState.save();
+          Get.snackbar('작성 완료', '물품 리뷰가 등록되었습니다.');
+          print(this.data.reviewText);
+          print(this.data.rating);
+        }
+      }),
     );
   }
 
-  Widget admitButton() {
+  Widget admitButton(callback) {
     return InkWell(
-        child: Container(
-          padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-          width: Get.width,
-          height: Get.height * 0.081,
-          color: mainRed,
-          child: Center(
-              child: Text(
-            "작성완료",
-            style: TextStyle(
-                color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
-          )),
-        ),
-        onTap: () async {
-          // if (_formKey.currentState.validate()) {
-          //   _formKey.currentState.save();
-          //   await getOrCreateInitAPIData(
-
-          Get.back();
-        });
+      child: Container(
+        padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+        width: Get.width,
+        height: Get.height * 0.081,
+        color: mainRed,
+        child: Center(
+            child: Text(
+          "작성완료",
+          style: TextStyle(
+              color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+        )),
+      ),
+      onTap: callback,
+    );
   }
 
   AppBar buildAppBar() {
